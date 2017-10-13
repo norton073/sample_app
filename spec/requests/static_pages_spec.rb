fe3a340bc67eed371d67bee6be87cb358f644b28
norton <norton.grr@gmail.com>
2017-10-13 16:12:55 +0400

    Add Spec tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,33 @@
+# require 'spec_helper'
+require 'rails_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+end
+
+# require 'rails_helper'
+#
+# RSpec.describe "StaticPages", type: :request do
+#   describe "GET /static_pages" do
+#     it "works! (now write some real specs)" do
+#       # get static_pages_index_path
+#       visit '/static_pages/home'
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+# end
