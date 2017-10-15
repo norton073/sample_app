9d1c5303237c2fc49da17f0ccf31181e5da32a57
norton <norton.grr@gmail.com>
2017-10-15 14:49:19 +0400

    Refactoring Rspec test, add utilities.rb file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,10 @@
 # require 'spec_helper'
 require 'rails_helper'
+require 'support/utilities.rb'
 
 describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+#  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   subject { page }
 
@@ -11,7 +12,7 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title("#{base_title}") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
@@ -19,21 +20,21 @@ describe "Static pages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help") }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About') }
-    it { should have_title("#{base_title} | About Us") }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it { should have_title(full_title('Contact')) }
   end
 end
 
