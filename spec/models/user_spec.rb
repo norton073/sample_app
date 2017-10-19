91d6a839174e3224d96fe4354ef704955bbe5eca
norton <norton.grr@gmail.com>
2017-10-19 12:22:37 +0400

    Add additional test validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,8 +19,10 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  # The same. Example
   # it "should be valid" do
   #   expect(@user).to be_valid
   # end
@@ -91,6 +93,32 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+
+    # Small debug
+    # it { p(@user.password)
+    # should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_falsey }
+    end
+  end
+
 end
 
 # RSpec.describe User, type: :model do
