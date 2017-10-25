ea33b54af03dba22db15ce8f067a8184114e438b
norton <norton.grr@gmail.com>
2017-10-25 16:33:19 +0400

    Add rspec 'Authentication' test

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+end
+
+
+# RSpec.describe "AuthenticationPages", type: :request do
+#   describe "GET /authentication_pages" do
+#     it "works! (now write some real specs)" do
+#       get authentication_pages_index_path
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+# end
