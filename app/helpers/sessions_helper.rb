e1b18a4dc62ceb47538a8787c4ffc213ccd9b0b7
norton <norton.grr@gmail.com>
2017-10-26 11:55:45 +0400

    Add sign_in function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    # The same
+    # cookies[:remember_token] = { value: remember_token, expires: 20.years.from_now.utc }
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
+  end
 end
