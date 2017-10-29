0d0a29b79820336a64830ff832a92d8358c3dd27
norton <norton.grr@gmail.com>
2017-10-29 13:12:53 +0400

    Users. Add 'signed_in_user' func, use for 'before_action'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -36,5 +38,17 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before filters
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+
+    # The same
+    # unless signed_in?
+    #   flash[:notice] = "Please sign in."
+    #   redirect_to signin_url
+    # end
   end
 end
